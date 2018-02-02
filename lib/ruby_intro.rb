230cdec09a2239e63539494dd12a69b9e22723c3
Alvin <Alvin>
2018-02-01 19:19:13 -0600

    finished part 1 and 2

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,56 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  return arr.reduce(0,:+)
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if (arr.length == 0)
+    return 0
+  else
+    sum(arr.sort.last(2))
+  end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  arr.combination(2).to_a.each do |a|
+  if (a.reduce(0, :+) == n)
+    return true
+  end
+end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  vowel = ['a','e','i','o','u']
+  if (s.length == 0)
+    return false
+  elsif (!s.downcase[0].match(/^[[:alpha:]]$/))
+    return false
+  elsif (!vowel.include?(s.downcase[0]))
+    return true
+  end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if (s !~ /^[01]/)
+    return false
+  elsif (s.to_i(10) % 4 == 0)
+    return true
+  else
+    return false
+  end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  :isbn
+  :price
 end
